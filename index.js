99bc309d8da8e73c8029e8df55a4fc04e74e43ec
Natalie Triffo <nattriffo@gmail.com>
2022-04-05 18:13:34 -0500

    adding questions and write to file

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 // TODO: Include packages needed for this application
+const fs = require ("fs")
+const inquirer = require ("inquirer")
+const generateMD = require ("./utils/generateMarkdown")
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message:"What is the title of your project",
+    },
+    {
+        type: "input",
+        name: "description",
+        message:"What is the description of your project",
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+return fs.writeFileSync(path.join(process.cwd(),fileName),data)
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    
+}
 
 // Function call to initialize app
 init();
